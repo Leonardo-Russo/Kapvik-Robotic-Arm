e32159a062cd68b48ded542181f2e42cd5686094
Nxze <leonardo.rxsso@gmail.com>
2023-11-16 12:13:31 +0100

    created workspace plot

--- Library/plot3DBox.m
+++ Library/plot3DBox.m
@@ -1,11 +1,19 @@
-function plot3DBox(length, width, height)
+function plot3DBox(length, width, height, facecolor)
 % Description: this function draws a box in 3D space. The reference frame
 % is coherent with the choice of station frame in this case study.
+
+if nargin < 4
+    facecolor = [219, 219, 219];
+    facecolor = facecolor / norm(facecolor);
+end
+
+edgecolor = [82, 82, 82];
+edgecolor = edgecolor / norm(edgecolor);
 
 % Define the vertices of the box
 % The origin is on the bottom, at the center of one of the short sides
 x = [-width/2, width/2, width/2, -width/2, -width/2, width/2, width/2, -width/2];
-y = [0, 0, length, length, 0, 0, length, length];
+y = -[0, 0, length, length, 0, 0, length, length];
 z = [0, 0, 0, 0, height, height, height, height];
 
 % Define the vertices of each face
@@ -16,13 +24,7 @@ faces = [1, 2, 6, 5;    % Front face
          1, 2, 3, 4;    % Bottom face
          5, 6, 7, 8];   % Top face
 
-% Create the figure and plot the box
-figure;
-patch('Vertices',[x',y',z'], 'Faces',faces, 'FaceColor','blue','EdgeColor','black');
-xlabel('Width');
-ylabel('Length');
-zlabel('Height');
-axis equal;
-grid on;
-view(3); % Set to 3D view
+% Plot the Box
+patch('Vertices',[x',y',z'], 'Faces',faces, 'FaceColor', facecolor, 'EdgeColor', edgecolor, 'FaceAlpha', 0.55);
+
 end
--- main.m
+++ main.m
@@ -34,9 +34,6 @@ X = simplify(trans2pose(T_T2S));
 
 J = simplify(jacobian(X, [q1 q2 q3 q4]));
 
-% TableMDH = double(subs(TableMDH, [q1, q2, q3, q4], [pi/2, 0, 0, 0]));
-% showTable(TableMDH)
-
 %% Define links and joints properties
 
 Upper_Arm=link(0.46, 40, 2, 3.5);
@@ -50,21 +47,23 @@ Joint_4=joint(0.67, 6.7,  -90,   5, 1.5*10^(-4), -5*10^(-4));
 
 %% Plots
 
-% close all
-% 
-% figure('name', 'Workspace')
-% 
-% % plot3(0, 0, 0, 'b*')    % Station Origin
-% show_frame(zeros(6, 1), 'b')
-% xlabel('x')
-% ylabel('y')
-% zlabel('z')
-% hold on
-% show_frame([P_B; zeros(3, 1)], 'r')
-% 
-% grid on
-% axis([-1 1 -1 1 -1 1])
-% show_frame(X, '#eb34e5')
+close all
 
-plot3DBox(10, 5, 3);
+% Compute Necessary Variables
+TableMDH = double(subs(TableMDH, [q1, q2, q3, q4], [pi/2, 0, 0, 0]));
+showTable(TableMDH)
+T_W2B = dir_kine(TableMDH);
+T_T2S = where_fun(T_S2B, T_W2B, T_T2W);
+X = trans2pose(T_T2S);
+
+% Create the Workspace
+figure('name', 'Workspace Test')
+show_frame(zeros(6, 1), 'k', "S")                   % Station frame
+show_frame([P_B; zeros(3, 1)], '#2b31ed', "B")      % Base frame
+show_frame(X, '#e84f1c', "T")                       % Tool frame
+plot3DBox(L, 0.5, h);                               % Create the Box
+
+
+
+
 
