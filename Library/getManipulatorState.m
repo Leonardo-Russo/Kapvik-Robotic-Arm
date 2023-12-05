526d35650e3348b5d6e3cf2035c1f3caaa8f3818
Nxze <leonardo.rxsso@gmail.com>
2023-12-05 10:22:27 +0100

    teach plot v1.0

---
+++ Library/getManipulatorState.m
@@ -0,0 +1,38 @@
+function [T_12S, T_22S, T_32S, T_W2S, T_T2S, X_S, X_B, X_W, X_T, X_1, X_2, X_3] = ...
+          getManipulatorState(Q, TableMDHsym, T_W2Bsym, X_Tsym, T_B2S, T_T2W)
+% Description: this function evaluates the manipulator state from its
+% joint variables state Q.
+
+global Qsym
+q1 = Qsym(1);
+q2 = Qsym(2);
+q3 = Qsym(3);
+q4 = Qsym(4);
+
+% Compute the Symbolic Quantities
+TableMDH = double(subs(TableMDHsym, [q1, q2, q3, q4], [Q(1), Q(2), Q(3), Q(4)]));
+T_W2B = double(subs(T_W2Bsym, [q1, q2, q3, q4], [Q(1), Q(2), Q(3), Q(4)]));
+X_T = double(subs(X_Tsym, [q1, q2, q3, q4], [Q(1), Q(2), Q(3), Q(4)]));
+
+% Compute Transformation Matrices
+T_12B = tableRow2T(TableMDH(1, :));
+T_12S = T_B2S * T_12B;
+T_221 = tableRow2T(TableMDH(2, :));
+T_22S = T_B2S * T_12B * T_221;
+T_322 = tableRow2T(TableMDH(3, :));
+T_32S = T_B2S * T_12B * T_221 * T_322;
+T_W23 = tableRow2T(TableMDH(4, :));
+T_W2S = T_B2S * T_12B * T_221 * T_322 * T_W23;
+T_T2S = T_B2S * T_12B * T_221 * T_322 * T_W23 * T_T2W;
+
+% Compute Poses
+X_S = zeros(6, 1);
+X_B = trans2pose(T_B2S);
+X_W = trans2pose(T_W2S);
+
+X_1 = trans2pose(T_12S);
+X_2 = trans2pose(T_22S);
+X_3 = trans2pose(T_32S);
+
+
+end
---
+++ Library/updatePlot.m
@@ -0,0 +1,42 @@
+function updatePlot(src, event, Q, TableMDHsym, T_W2Bsym, X_Tsym, T_B2S, T_T2W, joints, links, scoop, mframes, jframes, options, Upper_Arm, Fore_Arm, scoopLength)
+
+global Qsym
+q1 = Qsym(1);
+q2 = Qsym(2);
+q3 = Qsym(3);
+q4 = Qsym(4);
+
+jointIndex = str2double(src.Tag(2));        % extract joint index from the tag
+newAngle = deg2rad(get(src, 'Value'));      % get the new value from the slider
+Q(jointIndex) = newAngle;                   % update the corresponding joint variable
+
+% Compute new Manipulator State
+[T_12S, T_22S, T_32S, T_W2S, T_T2S, X_S, X_B, X_W, X_T, X_1, X_2, X_3] = ...
+    getManipulatorState(Q, TableMDHsym, T_W2Bsym, X_Tsym, T_B2S, T_T2W);
+
+
+% Update the Plot
+update_joint(joints.J1, T_12S);
+update_joint(joints.J2, T_22S);
+update_joint(joints.J3, T_32S);
+update_joint(joints.J4, T_W2S);
+
+update_link(links.arm, T_22S, Upper_Arm);
+update_link(links.forearm, T_32S, Fore_Arm);
+
+update_scoop(scoop, scoopLength, T_T2S);
+
+if options.show_frames
+    update_frame(mframes.S, X_S);
+    update_frame(mframes.B, X_B);
+    update_frame(mframes.W, X_W);
+    update_frame(mframes.T, X_T);
+
+    update_frame(jframes.J1, X_1);
+    update_frame(jframes.J2, X_2);
+    update_frame(jframes.J3, X_3);
+end
+
+drawnow;    % visually update the plot
+
+end
